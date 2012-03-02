8e6059fd353e24c73589918f8f0f1ab5867cd93f
Dennis Reimann <mail@dennisreimann.de>
2012-03-02 20:11:58 +0100

    Deploy github version

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :user, "deploy"
 set :application, "codeshelver"
 set :domain, "innovated.de"
 set :deploy_to, "/var/www/#{application}"
-set :repository, "git@git.innovated.de:#{application}.git"
+set :repository, "git@github.com:dbloete/Codeshelver.git"
 set :copy_shared, { 'config/app.js' => 'config/app.js' }
 set :symlinks, { 'config/app.js' => 'config/app.js' }
 set :revision, "origin/master"
