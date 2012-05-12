081389afb916720502c67ff2fee00fb520632dbf
Dennis Reimann <mail@dennisreimann.de>
2012-05-12 07:56:19 +0200

    Added local userscript for development and bumped version

--- public/javascripts/codeshelver.development.user.js
+++ public/javascripts/codeshelver.development.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        Codeshelver
+// @name        Codeshelver (development)
 // @description Cleans up your GitHub watchlist and gives you a place to remember repositories
 // @include     https://github.com/*
 // @include     http://github.com/*
@@ -12,7 +12,7 @@
   // added or GitHub changes something (like their markup). Also
   // disables the buttons in case codeshelver.com is not available.
   var script = document.createElement('script');
-  script.src = 'https://www.codeshelver.com/javascripts/userscript.js?v=2012051101';
+  script.src = 'http://codeshelver.dev:3000/javascripts/userscript.js?v=1012051101';
   script.type = 'text/javascript';
   document.getElementsByTagName('head')[0].appendChild(script);
 })();
